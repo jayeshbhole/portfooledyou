4efb7309b92757f0389a46cd51f302115d644883
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-04-26 10:12:00 +0530

    About

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import "../scss/about.scss";
+const About = () => {
+	return (
+		<div id="About" className="section">
+			<h2>About Me</h2>
+			<p>
+				Hello!
+				<br />
+				I am Jayesh Bhole, an undergraduate at IIIT Pune, full-stack web developer, robotics
+				enthusiast, a keen reader and a fan of personal growth.
+				<br />
+				On the edge of my seat to learn and create new stuff.
+			</p>
+		</div>
+	);
+};
+
+export default About;
