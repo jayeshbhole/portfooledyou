17a1de739197e946bfe4bea573cd824d38da73f5
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-05-24 22:27:30 +0530

    Responsive ✅

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,35 @@
 import "../scss/navbar.scss";
 import { HashLink } from "react-router-hash-link";
-
+import { HiMenuAlt1 } from "react-icons/hi";
+import { IoCloseOutline } from "react-icons/io5";
+import { useState } from "react";
 const Navbar = () => {
+	const [show, setShow] = useState(false);
+
 	return (
-		<nav>
-			<span>[</span>
+		<nav className={`${show}`}>
+			<div className={`nav ${show}`} onClick={() => setShow(false)}>
+				<span>[</span>
 
-			<HashLink smooth={true} to="/#">
-				"Home",
-			</HashLink>
-			<HashLink smooth={true} to="/#About">
-				"About",
-			</HashLink>
-			<HashLink smooth to="/#Projects">
-				"Projects",
-			</HashLink>
-			<HashLink smooth to="/#Contact">
-				"Contact"
-			</HashLink>
+				<HashLink smooth={true} to="/#">
+					"Home",
+				</HashLink>
+				<HashLink smooth={true} to="/#About">
+					"About",
+				</HashLink>
+				<HashLink smooth to="/#Projects">
+					"Projects",
+				</HashLink>
+				<HashLink smooth to="/#Contact">
+					"Contact"
+				</HashLink>
 
-			<span>]</span>
+				<span>]</span>
+			</div>
+			<span id="menu" onClick={() => setShow(!show)}>
+				<HiMenuAlt1 className={`${!show}`} />
+				<IoCloseOutline className={`${show}`} />
+			</span>
 		</nav>
 	);
 };
