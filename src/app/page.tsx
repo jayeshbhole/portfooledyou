2a522c825f32d6408479fbcc728488585b02f580
Jayesh Bhole <jayeshbhole123@gmail.com>
2024-08-07 04:20:57 +0530

    style: edit section styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,68 +4,16 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import RandomSection from "@/components/RandomSection";
-
-interface ExperienceItem {
-  company: string;
-  position: string;
-  duration: string;
-  description: string[];
-}
-
-interface ProjectItem {
-  name: string;
-  date: string;
-  description: string[];
-}
-
-interface AppData {
-  name: string;
-  title: string;
-  about: string;
-  experience: ExperienceItem[];
-  projects: ProjectItem[];
-}
-
-const dummyData: AppData = {
-  name: "Jayesh",
-  title: "Developer (Self && Web3)",
-  about:
-    "I want to be part of the reason your users tweet about how good the app feels. Why? Cause it puts a stupidly wide smile on my face :)",
-  experience: [
-    {
-      company: "StakeEase",
-      position: "Founding Engineer & Designer",
-      duration: "Jan 2024 - Current",
-      description: [
-        "Designed and built the user interface for stakeease.com to facilitate smoother and 60% cheaper restaking transactions.",
-        "Gathered insights with PostHog about user flows, UI interactions and usage patterns.",
-        "Worked on indexing defi positions of users for Uniswap V3-like liquidity pools.",
-      ],
-    },
-    // Add more experience items here
-  ],
-  projects: [
-    {
-      name: "One - AA Human Wallet",
-      date: "Apr 2023",
-      description: [
-        "Built at EthIndia Fellowship 3.0",
-        "ERC4337 Wallet with SMS onboarding and account usernames",
-        "On-chain session keys for multi device login and spending limits for sessions",
-      ],
-    },
-    // Add more projects here
-  ],
-};
+import { appData } from "@/constants/data";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-black text-white">
-      <div className="container mx-auto px-4">
-        <Header name={dummyData.name} title={dummyData.title} />
-        <About about={dummyData.about} />
-        <Experience experience={dummyData.experience} />
-        <Projects projects={dummyData.projects} />
+    <div className="min-h-screen">
+      <div className="container mx-auto mt-16 flex max-w-[80ch] flex-col gap-10 px-4 print:mt-4 print:max-w-max">
+        <Header name={appData.name} />
+        <About about={appData.about} />
+        <Experience experience={appData.experience} />
+        <Projects projects={appData.projects} />
         <RandomSection />
       </div>
     </div>
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { ReactElement } from "react";
 
 interface AboutProps {
-  about: string;
+  about: ReactElement;
 }
 
 const About: React.FC<AboutProps> = ({ about }) => {
   return (
-    <section className="my-8">
-      <h2 className="mb-4 text-2xl font-bold">About Me</h2>
-      <p className="text-gray-300">{about}</p>
+    <section className="flex flex-col gap-2">
+      {/* <h2 className="item-cell text-2xl font-semibold tracking-wide">
+        <span>About Me</span>
+      </h2> */}
+
+      <div className="item-cell">
+        <q className="relative pl-4 before:absolute before:inset-y-1/2 before:left-0 before:inline-block before:h-[100%] before:w-1 before:-translate-y-1/2 before:items-center before:justify-center before:rounded-full before:bg-neutral-300 before:content-[''] dark:before:bg-neutral-700">
+          {about}
+        </q>
+      </div>
     </section>
   );
 };
