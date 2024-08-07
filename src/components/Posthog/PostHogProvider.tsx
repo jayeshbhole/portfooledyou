429132cbf6b787fbd30435c526b5d0b4a6faa82a
Jayesh Bhole <jayeshbhole123@gmail.com>
2024-08-07 14:04:57 +0530

    refactor: posthog

--- src/components/Posthog/PostHogProvider.tsx
+++ src/components/Posthog/PostHogProvider.tsx
@@ -6,8 +6,10 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: "/ingest",
+    ui_host: "https://us.i.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
   });
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
