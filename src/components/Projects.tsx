37f51d4f4b38e9a73e742724af87b53f7b222e87
Jayesh Bhole <jayeshbhole123@gmail.com>
2024-08-07 13:09:47 +0530

    feat(projects): add tech stack

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,6 @@
+import { ProjectItem } from "@/constants/data";
+import Link from "next/link";
 import React, { ReactNode } from "react";
-
-interface ProjectItem {
-  name: string;
-  date: string;
-  description: ReactNode[];
-}
 
 interface ProjectsProps {
   projects: ProjectItem[];
@@ -20,16 +16,18 @@ const Projects: React.FC<ProjectsProps> = ({ projects }) => {
       {/* make projects similar to experience map */}
       {projects.map((project, index) => (
         <>
-          <div key={index} className="my-1 flex flex-col">
+          <div key={index} className="my-1 flex flex-col gap-2">
             <div className="item-cell flex w-full justify-between whitespace-nowrap">
               <h3 className="flex flex-1 gap-1 text-base font-medium">
-                <span>{project.name}</span>
+                <Link href={project.link} target="_blank">
+                  <span>{project.name}</span>
+                </Link>
               </h3>
 
               <p className="text-right italic opacity-50">{project.date}</p>
             </div>
 
-            <ul className="mt-2 flex list-inside list-none flex-col gap-1">
+            <ul className="flex list-inside list-none flex-col gap-1">
               {project.description.map((item, i) => (
                 <li key={i} className="item-cell">
                   <div className="h-4 w-[2.5ch] self-start pt-3 leading-[1]">
@@ -40,6 +38,17 @@ const Projects: React.FC<ProjectsProps> = ({ projects }) => {
                 </li>
               ))}
             </ul>
+
+            <div className="flex flex-wrap gap-1">
+              {project.tech.map((tag, i) => (
+                <span
+                  key={i}
+                  className="rounded-md bg-neutral-100 px-2 py-1 text-xs text-neutral-700 dark:bg-neutral-800 dark:text-neutral-200"
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
           </div>
 
           <hr className="mb-1 border-t border-neutral-300/25 dark:border-neutral-700/25" />
